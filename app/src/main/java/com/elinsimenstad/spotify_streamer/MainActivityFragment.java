c99318975a44e0848c9b67813dcee4ee83c8bccf
Elin <elin_sim@htomail.com>
2015-06-12 20:45:56 +0200

    first commit

--- app/src/main/java/com/elinsimenstad/spotify_streamer/MainActivityFragment.java
+++ app/src/main/java/com/elinsimenstad/spotify_streamer/MainActivityFragment.java
@@ -24,6 +24,7 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
         /*
         String[] data = {
                 "Mon 6/23 - Sunny - 31/17",
